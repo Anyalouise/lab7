37fdc5f8b7c84cced05977c492626243a3230442
Anya Bouzida <anyalouise@gmail.com>
2021-02-18 10:55:19 -0800

    lab7 in class

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
+data = true
 exports.view = function(request, response){
+	projects["viewAlt"] = false;
+  	response.render('index', projects);
+};
+
+exports.viewAlt = function(request, response){
   	response.render('index', projects);
 };
